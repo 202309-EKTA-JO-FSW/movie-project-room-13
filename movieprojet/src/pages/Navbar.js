bfb152e8ee3d45fa86264b6dde018216e7de462e
Noor Alrai <ameinnoor91@gmail.com>
2023-12-05 21:46:34 +0300

    Movies.js

--- movieprojet/src/pages/Navbar.js
+++ movieprojet/src/pages/Navbar.js
@@ -1,50 +1,65 @@
-import React ,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+function Navbar({ onSearch }) {
+  const [genres, setGenres] = useState([]);
+  const [selectedType, setSelectedType] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
-function Navbar({onSearch}){
-    const [genres, setGenres] = useState([]);
-    const [selectedType, setSelectedType] = useState("");
-    const [selectedGenre, setSelectedGenre] = useState("");
-    const [searchTerm, setSearchTerm] = useState('');
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    onSearch(e.target.value);
+  };
 
-    const handleSearch = (e) => {
-      setSearchTerm(e.target.value);
-      onSearch(e.target.value);
+  const router = useRouter();
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYWNjNWNiN2YzN2EzNjZkNWNkNTVjYmE3NGI0M2ZlMiIsInN1YiI6IjY1Njc3NjZjMDIxY2VlMDEzYTg0MzMxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AzZZFUx9YA3TyFD4o86SQE1mY1CwZkk5Kx09BfWQZPY",
+      },
     };
-    const router = useRouter();
-    useEffect(() => {
-        const options = {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYWNjNWNiN2YzN2EzNjZkNWNkNTVjYmE3NGI0M2ZlMiIsInN1YiI6IjY1Njc3NjZjMDIxY2VlMDEzYTg0MzMxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AzZZFUx9YA3TyFD4o86SQE1mY1CwZkk5Kx09BfWQZPY',
-          },
-        };
-    
-        fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
-          .then(response => response.json())
-          .then(data => setGenres(data.genres))
-          .catch(error => console.error(error));
-      }, []);
-  
-      const handleTypeChange = (e) => {
-        const type = e.target.value;
-        setSelectedType(type);
-        router.push(`/movies/${type}`);
-      };
-      const handleGenreChange = (e) => {
-        const genre = e.target.value;
-        setSelectedGenre(genre);
-        router.push(`/movies/${selectedType}/${genre}`);
 
+    fetch(
+      "https://api.themoviedb.org/3/genre/movie/list?language=en",
+      options
+    )
+      .then((response) => response.json())
+      .then((data) => setGenres(data.genres))
+      .catch((error) => console.error(error));
+  }, []);
 
-      };
-   return (<nav style={{display:"flex", width:"100vw" ,justifyContent:"center", backgroundColor:"whitesmoke" }}>
-   <Link href="/" className="logo" style={{padding: "5px"}}>
+  const handleTypeChange = (e) => {
+    const type = e.target.value;
+    setSelectedType(type);
+    router.push(`/movies/${type}`);
+  };
 
-<img src="https://png.pngtree.com/png-clipart/20200225/ourlarge/pngtree-movie-icon-design-png-image_2153114.jpg" style={{width:"50px"}} />
+  const handleGenreChange = (e) => {
+    const genre = e.target.value;
+    setSelectedGenre(genre);
+    router.push(`/movies/${selectedType}/${genre}`);
+  };
+
+  return (
+    <nav
+      style={{
+        display: "flex",
+        width: "100vw",
+        justifyContent: "center",
+        backgroundColor: "whitesmoke",
+      }}
+    >
+      <Link href="/" className="logo" style={{ padding: "5px" }}>
+        <img
+          src="https://png.pngtree.com/png-clipart/20200225/ourlarge/pngtree-movie-icon-design-png-image_2153114.jpg"
+          style={{ width: "50px" }}
+        />
       </Link>
 
       <div className="genres-dropdown" style={{ padding: "10px" }}>
@@ -65,22 +80,25 @@ function Navbar({onSearch}){
           <option value="popular">Popular</option>
           <option value="upcoming">Up Coming</option>
           <option value="now_playing">Now Playing</option>
-         
         </select>
-
       </div>
-    
-      <Link href="/Actors" style={{padding: "10px"}}>
+
+      <Link href="/Movies" style={{ padding: "10px" }}>
+        Movies
+      </Link>
+      <Link href="/Actors" style={{ padding: "10px" }}>
         Actors
       </Link>
-    
-      <input style={{height:"25px"}}
+
+      <input
+        style={{ height: "25px" }}
         type="text"
         placeholder="Search movies..."
         value={searchTerm}
         onChange={handleSearch}
       />
-      
-    </nav>)
+    </nav>
+  );
 }
-export default Navbar
+
+export default Navbar;
