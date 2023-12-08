c96f8eb9fe843b7b555c6674f15076a7f87f05c6
wajd alkayyali <wajd.kayyali@gmail.com>
2023-12-08 11:31:12 +0300

    finished only the links and the read me file

--- movieprojet/src/pages/Footer.jsx
+++ movieprojet/src/pages/Footer.jsx
@@ -8,9 +8,9 @@ config.autoAddCss = false;
 const Footer = () => {
   const teamMembers = [
     { name: `Noor Alra'i`, github: 'https://github.com/Noor-Alrai', linkedin: 'https://www.linkedin.com/in/noor-alrai/' },
-    { name: 'Lubna Abdalkhaliq', github: 'teammate1-github-link', linkedin: 'teammate1-linkedin-link' },
-    { name: 'Wajd Al-Kayyali', github: 'teammate2-github-link', linkedin: 'teammate2-linkedin-link' },
-    { name: 'Lin Daaboul', github: 'teammate2-github-link', linkedin: 'teammate2-linkedin-link' },
+    { name: 'Lubna Abdalkhaliq', github: 'https://github.com/Lubnabd', linkedin: 'https://www.linkedin.com/in/lubna-abdelkhaliq-09020a18b/' },
+    { name: 'Wajd Al-Kayyali', github: 'https://github.com/Wajd-AlKayyali', linkedin: 'https://www.linkedin.com/in/wajd-al-kayyali/' },
+    { name: 'Lin Daaboul', github: 'https://github.com/leen-gh/', linkedin: 'https://www.linkedin.com/in/leen-ghalib-398b3815a/' },
 
   ];
 
